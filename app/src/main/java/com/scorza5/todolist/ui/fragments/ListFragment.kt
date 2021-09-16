062da98057469989fa33e88a589818418147520f
Bruno Scorzafave <bruno.scorzafave@aluno.ufabc.edu.br>
2021-09-16 20:06:20 -0300

    Empty State added

--- app/src/main/java/com/scorza5/todolist/ui/fragments/ListFragment.kt
+++ app/src/main/java/com/scorza5/todolist/ui/fragments/ListFragment.kt
@@ -1,21 +1,14 @@
 package com.scorza5.todolist.ui.fragments
 
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.FragmentManager
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.whenStarted
 import androidx.navigation.fragment.findNavController
 import com.scorza5.todolist.R
 import com.scorza5.todolist.databinding.FragmentListBinding
-import com.scorza5.todolist.model.Task
-import com.scorza5.todolist.ui.DiffCallback
 import com.scorza5.todolist.ui.TaskListAdapter
 import com.scorza5.todolist.viewmodel.TaskViewModel
 
@@ -38,6 +31,7 @@ class ListFragment : Fragment() {
         mTaskViewModel = ViewModelProvider(this).get(TaskViewModel::class.java)
 
         updateList()
+        emptyState(false)
 
         adapter.listenerEdit = {
             val bundle = Bundle(1)
@@ -58,8 +52,17 @@ class ListFragment : Fragment() {
     private fun updateList(){
 
         mTaskViewModel.readAllData.observe(viewLifecycleOwner, { task ->
+            if (task.isNotEmpty()){
+                emptyState(false)
+            } else{
+                emptyState(true)
+            }
             adapter.submitList(task)
         })
 
+    }
+
+    private fun emptyState(visibility: Boolean){
+        binding.includeEmpty.root.visibility = if (visibility) View.VISIBLE else View.GONE
     }
 }
